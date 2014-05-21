a0cc76b8faa661d933dde93042a8229e02483cbf
mic4ael <crossner90@gmail.com>
2014-05-21 22:04:32 +0200

    Inicjalizacja widoku i kilku obiektów

--- src/dmcs/project/cameraapp/MainActivity.java
+++ src/dmcs/project/cameraapp/MainActivity.java
@@ -1,64 +1,81 @@
 package dmcs.project.cameraapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends ActionBarActivity {
-
+	private VideoView videoView;
+	private MediaController mediaController;
+	private Uri cameraUri;
+	private static final String url = "https://212.51.218.248/video.cgi";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+		
+		try {
+			initUrl();
+			initVideoView();
+		} catch (IllegalArgumentException | SecurityException
+				| IllegalStateException | IOException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	private void initUrl() {
+		Authenticator.setDefault(new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("root", "test.123".toCharArray());
+			}
+		});
+		
+		cameraUri = Uri.parse(url);
+	}
+
+	private void initVideoView() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
+		mediaController = new MediaController(this);
+		mediaController.setAnchorView(videoView);
+		videoView = (VideoView) findViewById(R.id.videoViewId);
+		videoView.setVideoURI(cameraUri);
+		videoView.setOnPreparedListener(new OnPreparedListener() {
+			
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+				videoView.start();
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
