33c7f9a43c3fdf9e6e2cb8b48a2c3b4b8a86f5ed
mic4ael <crossner90@gmail.com>
2014-05-27 21:56:25 +0200

    Dodane trzy klasy, które powinny załatwić sprawę

--- src/dmcs/project/cameraapp/MainActivity.java
+++ src/dmcs/project/cameraapp/MainActivity.java
@@ -1,67 +1,81 @@
 package dmcs.project.cameraapp;
 
 import java.io.IOException;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
+import java.net.URI;
 
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnPreparedListener;
-import android.net.Uri;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.view.Window;
+import android.view.WindowManager;
+import dmcs.project.cameraapp.mjpeg.MJPegInputStream;
+import dmcs.project.cameraapp.mjpeg.MJPegView;
 
 public class MainActivity extends ActionBarActivity {
-	private VideoView videoView;
-	private MediaController mediaController;
-	private Uri cameraUri;
-	private static final String url = "https://212.51.218.248/video.cgi";
+	private static final String url = "http://212.51.218.248/video.cgi";
+	private MJPegView mv;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+							 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
-		try {
-			initUrl();
-			initVideoView();
-		} catch (IllegalArgumentException | SecurityException
-				| IllegalStateException | IOException e) {
-			e.printStackTrace();
+		mv = new MJPegView(this);
+		setContentView(mv);
+		
+		new DoRead().execute(url);
+	}
+	
+	public class DoRead extends AsyncTask<String, Void, MJPegInputStream> {
+
+		@Override
+		protected MJPegInputStream doInBackground(String... url) {
+			HttpResponse res = null;
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			httpClient.getCredentialsProvider().setCredentials(
+												new AuthScope("https://212.51.218.248/", AuthScope.ANY_PORT), 
+												new UsernamePasswordCredentials("root", "test.123"));
+			
+			try {
+				res = httpClient.execute(new HttpGet(URI.create(url[0])));
+				if (res.getStatusLine().getStatusCode() == 401) {
+					return null;
+				}
+				
+				return new MJPegInputStream(res.getEntity().getContent());
+			} catch (ClientProtocolException ex) {
+				Log.d(this.getClass().toString(), ex.toString());
+			} catch (IOException ex) {
+				Log.d(this.getClass().toString(), ex.toString());
+			}
+			
+			return null;
 		}
+		
+		@Override
+		protected void onPostExecute(MJPegInputStream result) {
+			mv.setSource(result);
+			mv.setDispMode(MJPegView.SIZE_BEST_FIT);
+			mv.setShowFps(true);
+		}
+		
 	}
 	
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-	}
-
-	private void initUrl() {
-		Authenticator.setDefault(new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("root", "test.123".toCharArray());
-			}
-		});
-		
-		cameraUri = Uri.parse(url);
-	}
-
-	private void initVideoView() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
-		mediaController = new MediaController(this);
-		mediaController.setAnchorView(videoView);
-		videoView = (VideoView) findViewById(R.id.videoViewId);
-		videoView.setVideoURI(cameraUri);
-		videoView.setOnPreparedListener(new OnPreparedListener() {
-			
-			@Override
-			public void onPrepared(MediaPlayer mp) {
-				videoView.start();
-			}
-		});
 	}
 
 	@Override
@@ -78,5 +92,11 @@ public class MainActivity extends ActionBarActivity {
 		}
 		
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mv.stopPlayback();
 	}
 }
---
+++ src/dmcs/project/cameraapp/mjpeg/MJPegView.java
@@ -0,0 +1,249 @@
+package dmcs.project.cameraapp.mjpeg;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MJPegView extends SurfaceView implements SurfaceHolder.Callback {
+	public static final int POSITION_UPPER_LEFT = 9;
+	public static final int POSITION_UPPER_RIGHT = 3;
+	public final static int POSITION_LOWER_LEFT = 12;
+	public final static int POSITION_LOWER_RIGHT = 6;
+	
+	public final static int SIZE_STANDARD = 1;
+	public final static int SIZE_BEST_FIT = 4;
+	public final static int SIZE_FULLSCREEN = 8;
+	
+	private MJPegViewThread thread;
+	private MJPegInputStream mIn = null;
+	private boolean showFps = false;
+	private boolean mRun = false;
+	private boolean surfaceDone = false;
+	private Paint overlayPaint;
+	private int overlayTextColor;
+	private int overlayBgColor;
+	private int ovlPos;
+	private int dispWidth;
+	private int dispHeight;
+	private int dispMode;
+	
+	public class MJPegViewThread extends Thread {
+		private SurfaceHolder surfaceHolder;
+		private int frameCounter = 0;
+		private long start;
+		private Bitmap ovl;
+		
+		public MJPegViewThread(SurfaceHolder sh, Context ctx) {
+			this.surfaceHolder = sh;
+		}
+		
+		@Override
+		public void run() {
+			start = System.currentTimeMillis();
+			PorterDuffXfermode mode = new PorterDuffXfermode(PorterDuff.Mode.DST_OVER);
+			Bitmap bm;
+			int width;
+			int height;
+			Rect destRect;
+			Canvas c = null;
+			Paint p = new Paint();
+			String fps;
+			
+			while(mRun) {
+				if (surfaceDone) {
+					try {
+						c = surfaceHolder.lockCanvas();
+						synchronized(surfaceHolder) {
+							try {
+								bm = mIn.readMjpegFrame();
+								destRect = destRect(bm.getWidth(), bm.getHeight());
+								c.drawColor(Color.BLACK);
+								c.drawBitmap(bm, null, destRect, p);
+								
+								if (showFps) {
+									p.setXfermode(mode);
+									if (ovl != null ) {
+										height = ((ovlPos & 1) == 1) ? destRect.top : destRect.bottom - ovl.getHeight(); 
+										width = ((ovlPos & 8) == 8) ? destRect.left : destRect.right - ovl.getWidth();
+										c.drawBitmap(ovl, width, height, null);
+									}
+									
+									p.setXfermode(null);
+									frameCounter++;
+									if ((System.currentTimeMillis() - start) >= 1000) {
+										fps = frameCounter + " FPS";
+										frameCounter = 0;
+										start = System.currentTimeMillis();
+										ovl = makeFpsOverlay(overlayPaint, fps);
+									}
+								}
+							} catch (IOException ex) {
+								Log.d(this.getClass().toString(), ex.toString());
+							}
+						}
+					} finally {
+						if (c != null)
+							surfaceHolder.unlockCanvasAndPost(c);
+					}
+				}
+			}
+		};
+		
+		private Rect destRect(int bmw, int bmh) {
+			int tmpx;
+			int tmpy;
+			
+			if (dispMode == MJPegView.SIZE_STANDARD) {
+				tmpx = (dispWidth / 2) - (bmw / 2);
+				tmpy = (dispHeight / 2) - (bmh / 2);
+				return new Rect(tmpx, tmpy, bmw + tmpx, bmh + tmpy);
+			}
+			
+			if (dispMode == MJPegView.SIZE_BEST_FIT) {
+				float bmasp = (float) bmw / (float) bmh;
+				bmw = dispWidth;
+				bmh = (int) (dispWidth / bmasp);
+				
+				if (bmh > dispHeight) {
+					bmh = dispHeight;
+					bmw = (int) (dispHeight * bmasp);
+				}
+				
+				tmpx = (dispWidth / 2) - (bmw / 2);
+				tmpy = (dispHeight / 2) - (bmh / 2);
+				return new Rect(tmpx, tmpy, bmw + tmpx, bmh + tmpy);
+			}
+			
+			if (dispMode == MJPegView.SIZE_FULLSCREEN) {
+				return new Rect(0, 0, dispWidth, dispHeight);
+			}
+			
+			return null;
+		}
+		
+		public void setSurfaceSize(int w, int h) {
+			synchronized (surfaceHolder) {
+				dispWidth = w;
+				dispHeight = h;
+			}
+		}
+		
+		private Bitmap makeFpsOverlay(Paint p, String text) {
+			Rect b = new Rect();
+			p.getTextBounds(text, 0, text.length(), b);
+			int bwidth = b.width() + 2;
+			int bheight = b.height() + 2;
+			Bitmap bm = Bitmap.createBitmap(bwidth, bheight, Bitmap.Config.ARGB_8888);
+			Canvas c = new Canvas(bm);
+			p.setColor(overlayBgColor);
+			c.drawRect(0, 0, bwidth, bheight, p);
+			p.setColor(overlayTextColor);
+			c.drawText(text, -b.left + 1, (bheight / 2) - 
+					   ((p.ascent() + p.descent()) / 2)  +1, p);
+			return bm;
+		}
+	}
+	
+	
+	private void init(Context ctx) {
+		SurfaceHolder holder = getHolder();
+		holder.addCallback(this);
+		thread = new MJPegViewThread(holder, ctx);
+		setFocusable(true);
+		overlayPaint = new Paint();
+		overlayPaint.setTextAlign(Paint.Align.LEFT);
+		overlayPaint.setTextSize(12);
+		overlayTextColor = Color.WHITE;
+		overlayBgColor = Color.BLACK;
+		ovlPos = MJPegView.POSITION_LOWER_RIGHT;
+		dispMode = MJPegView.SIZE_STANDARD;
+		dispWidth = getWidth();
+		dispHeight = getHeight();
+	}
+	
+	public void startPlayback() {
+		if (mIn != null) {
+			mRun = true;
+			thread.start();
+		}
+	}
+	
+	public void stopPlayback() {
+		mRun = false;
+		boolean retry = false;
+		while(retry) {
+			try {
+				thread.join();
+				retry = false;
+			} catch (InterruptedException ex) {
+				Log.d(this.getClass().toString(), ex.toString());
+			}
+		}
+	}
+	
+	public MJPegView(Context ctx, AttributeSet attrs) {
+		super(ctx, attrs);
+		init(ctx);
+	}
+	
+	public MJPegView(Context context) {
+		super(context);
+		init(context);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int f, int w, int h) {
+		thread.setSurfaceSize(w, h);
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		surfaceDone = true;
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		surfaceDone = false;
+		stopPlayback();
+	}
+	
+	public void setSource(MJPegInputStream source) {
+		mIn = source;
+		startPlayback();
+	}
+	
+	public void setOverlayPaint(Paint p) {
+		overlayPaint = p;
+	}
+	
+	public void setOverlayTextColor(int c) {
+		overlayTextColor = c;
+	}
+	
+	public void setOverlayBgColor(int c) {
+		overlayBgColor = c;
+	}
+	
+	public void setOverlayPosition(int pos) {
+		ovlPos = pos;
+	}
+	
+	public void setDispMode(int mode) {
+		dispMode = mode;
+	}
+	
+	public void setShowFps(boolean fps) {
+		showFps = fps;
+	}
+} 
